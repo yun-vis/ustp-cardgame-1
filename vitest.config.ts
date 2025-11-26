bc4919d4bceea766b49d1667f1d1c2944fc8b5f6
Aruaci <laura.cesar3@gmail.comm>
2025-11-26 17:31:47 +0100

    Adds working unit test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,9 @@
-// vitest.config.ts
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
     globals: true,
-    setupFiles: './src/setupTests.ts', // We will create this next
-    reporters: ['default', 'junit'],   // <--- IMPORTANT: Generates the XML report for the AI task
-    outputFile: 'test-results.xml',    // <--- Where the report is saved
+    reporters: ['default', 'junit'],
+    outputFile: 'test-results.xml',
   },
 })
